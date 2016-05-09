4bdb0239081ecd1d2ae9b7a52473782b3a5ce4f1
JohnBeal <john_beal@paradise.net.nz>
2016-05-09 12:58:45 +1200

    input data cleaner to return bigram parsed from input text for trigram language model
    Modelled with and without stopwords:
    
    without: 0/10
    with: 3/10

--- Prediction.R
+++ Prediction.R
@@ -2,6 +2,23 @@
 
 library(dplyr)
 
+
+## Clean up input for prediction model ##
+
+input.clean <- function (x) {
+        require(tm)
+        x <- tolower(x)
+        x <- removePunctuation(x)
+        x <- removeNumbers(x)
+        #x <- removeWords(x, stopwords("english"))#
+        x <- stripWhitespace(x)
+        x <- unlist(strsplit(x, split = " "))
+        n <- length(x)
+        x <- paste(x[(n-1):n],  collapse = " ")
+        x
+}
+
+
 ## 3-gram model ##
 
 predict.ngram <- function (x = character, model, n = 1) {
