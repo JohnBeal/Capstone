99f9bd26bd3bfd20018659af930e1e16b26ba124
unknown <j.beal@CISS28251.int.irl.cri.nz>
2016-05-13 17:08:28 +1200

    Started rewriting prediction algorithm to make it more generally applicable to language models

--- Prediction.R
+++ Prediction.R
@@ -1,3 +1,13 @@
+## Generalised column names for all n-gram models##
+## where "n.gram" represents the relevant n-gram (i.e. trigram, bigram, unigram), "n_1.gram" represents the corresponding predictor
+## (i.e bigram, unigram) and "unigram" the outcome (which is always a unigram regardless of the level of the language model). 
+## "n.gram_count" and "n_1.gram_count" are the frequency counts of the predictor and outcome, and "prob.estimate" is the probability
+## estimate (i.e. maximum liklihood estimate (MLE) or some smoothed probability estimate).
+
+
+
+make.names(c("n.gram", "n_1.gram", "unigram", "n.gram_count", "n_1.gram_count", "prob.estimate"))
+
 ## Prediction alogrithm from n-gram model ##
 
 library(dplyr)
@@ -5,7 +15,8 @@ library(dplyr)
 
 ## Clean up input for prediction model ##
 
-input.clean <- function (x, m = 2) {
+
+input.clean <- function (x = character(0), m = 2) {
         require(tm)
         x <- tolower(x)
         x <- removePunctuation(x)
