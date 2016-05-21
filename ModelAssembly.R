0e86a9171eef5dcadc75525d3a25d526f7df175e
JohnBeal <john_beal@paradise.net.nz>
2016-05-22 10:46:10 +1200

    Sorted problesm with data frame assembly

--- ModelAssembly.R
+++ ModelAssembly.R
@@ -1,24 +1,36 @@
 ## 5. Model Assembly ##
 
-## Function to take first two words of trigram (wi-2, wi-1, wi) to generate corresponding bigram (wi-2, wi-1)##
+## Function to take first (n-1) words of n-gram ##
 
 n_n_1.gram <- function (x) {words <- unlist(strsplit(x, split = " "))
-                            n <- length(words)
-                            paste(words[1:(n-1)], collapse = " ")}
+n <- length(words)
+paste(words[1:(n-1)], collapse = " ")}
+
+## Function to take last word of n-gram XX
 
 n_unigram <- function (x) {words <- unlist(strsplit(x, split = " "))
-                           n <- length(words)
-                           words[n]}
+n <- length(words)
+words[n]}
+
+## Read in n-gram Term Frequncy Vectors from storage files ##
+
+TFV_trigram <- read.csv(file = ".\\Coursera-SwiftKey\\final\\en_US\\TFV_trigram_sw(-)_(bound(10)).csv", stringsAsFactors = F)
+names(TFV_trigram) <- c("trigram", "frequency")
+
+TFV_bigram <- read.csv( file = ".\\Coursera-SwiftKey\\final\\en_US\\TFV_bigram_sw(-)_(bound(10)).csv", stringsAsFactors = F)
+names(TFV_bigram) <- c("bigram", "frequency")
+
+TFV_unigram <- read.csv(file = ".\\Coursera-SwiftKey\\final\\en_US\\TFV_unigram_sw(-)_(bound(10)).csv", stringsAsFactors = F)
+names(TFV_unigram) <- c("unigram", "frequency")
 
 ## Trigram  ##
 
-uvw_trigram_pruned <- data.frame(n.gram = TFV_trigram_pruned["trigram"], 
-                                 n.gram_count = TFV_trigram_pruned["frequency"],
-                                 stringsAsFactors = FALSE)
-colnames(uvw_trigram_pruned) <- c("n.gram", "n.gram_count")
+uvw_trigram_pruned <- data.frame(n.gram = TFV_trigram[, "trigram"], 
+                                 n.gram_count = TFV_trigram[, "frequency"],
+                                 n_1.gram = sapply(TFV_trigram[, "trigram"], n_n_1.gram), 
+                                 unigram = sapply(TFV_trigram[, "trigram"], n_unigram),
+                                 stringsAsFactors = FALSE, row.names = NULL)
 
-
-uvw_trigram_pruned <- mutate(uvw_trigram_pruned, n_1.gram = n_n_1.gram(n.gram))
 
 ## Assemble data frame with bigrams and bigram counts from the TDM ##
 uv4 <- data.frame(trigram = names(myTDM_trigram_comb), trigramcount = myTDM_trigram_comb, stringsAsFactors = FALSE)
